b822eeeb02ca1f1fd180b6ef6c55a5e921150e2e
huangjienalahaaaa <137612270@qq.com>
2020-05-03 15:46:46 +0800

    f

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
-// 引入2组件
 import Index from './Pages/Index'
 import List from './Pages/List'
 
@@ -9,11 +7,14 @@ function AppRouter() {
     return (
         <Router>
             <ul>
+
                 <li> <Link to="/">首页</Link> </li>
-                <li><Link to="/list/">列表</Link> </li>
+                {/* 2. 设置好规则后，就可以在Link上设置值了 */}
+                <li><Link to="/list/123">列表</Link> </li>
             </ul>
             <Route path="/" exact component={Index} />
-            <Route path="/list/" component={List} />
+            {/* 1.设置传递规则-> 这个设置是以:开始的，然后紧跟着你传递的key（键名称）名称 */}
+            <Route path="/list/:id" component={List} />
         </Router>
     );
 }
--- src/Pages/List.js
+++ src/Pages/List.js
@@ -6,7 +6,21 @@ class List extends Component {
         this.state = {}
     }
     render() {
-        return (<h2>List Page</h2>);
+        //显示传递过来的id
+        return (<h2>List Page->{this.state.id}</h2>);
+    }
+    componentDidMount() {
+        /*
+        1. 组件接收传递过来的值的时候，可以在声明周期componentDidMount中进行，传递的值在this.props.match中。我们可以先打印出来,代码如下。
+        2. 然后在浏览器的控制台中可以看到打印出的对象，对象包括三个部分:
+            * patch:自己定义的路由规则，可以清楚的看到是可以产地id参数的。
+            * url: 真实的访问路径，这上面可以清楚的看到传递过来的参数是什么。
+            * params：传递过来的参数，key和value值。
+        明白了match中的对象属性，就可以轻松取得传递过来的ID值了。代码如下:
+        */
+        console.log(this.props.match)
+        let tempId = this.props.match.params.id
+        this.setState({ id: tempId })
     }
 }
 
