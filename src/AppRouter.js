0b6f6414268b0a63a34a564290b4377bd8bba808
huangjienalahaaaa <137612270@qq.com>
2020-05-03 15:34:32 +0800

    f

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,30 +1,21 @@
 import React from "react";
-// 引入浏览器路由
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-function Index() {
-    return <h2>JSPang.com</h2>;
-}
-
-function List() {
-    return <h2>List-Page</h2>;
-}
+// 引入2组件
+import Index from './Pages/Index'
+import List from './Pages/List'
 
 function AppRouter() {
     return (
         <Router>
             <ul>
-                {/* Link就是上面引入的，在页面上会渲染成<a>便签 */}
                 <li> <Link to="/">首页</Link> </li>
                 <li><Link to="/list/">列表</Link> </li>
             </ul>
-            {/* 
-            1. Route:上面定义了Link，下面要定义怎么跳转 
-            2. exact:精确匹配
-            */}
             <Route path="/" exact component={Index} />
             <Route path="/list/" component={List} />
         </Router>
     );
 }
+
 export default AppRouter;
