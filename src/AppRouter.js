b68aa4b7b1d5b4229f5eda984350f759ef0369e1
huangjienalahaaaa <137612270@qq.com>
2020-05-03 15:54:52 +0800

    f

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -9,11 +9,9 @@ function AppRouter() {
             <ul>
 
                 <li> <Link to="/">首页</Link> </li>
-                {/* 2. 设置好规则后，就可以在Link上设置值了 */}
                 <li><Link to="/list/123">列表</Link> </li>
             </ul>
             <Route path="/" exact component={Index} />
-            {/* 1.设置传递规则-> 这个设置是以:开始的，然后紧跟着你传递的key（键名称）名称 */}
             <Route path="/list/:id" component={List} />
         </Router>
     );
--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
+// 引入Link组件
+import { Link } from "react-router-dom";
 
 class Index extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        // 模拟一个列表数组
+        this.state = {
+            list: [
+                { uid: 123, title: '技术胖的个人博客-1' },
+                { uid: 456, title: '技术胖的个人博客-2' },
+                { uid: 789, title: '技术胖的个人博客-3' },
+            ]
+        }
     }
     render() {
-        return (<h2>JSPang.com</h2>);
+        return (
+            <ul>
+                {
+                    // 有了list数组后，再修改一下UI，进行有效的遍历，
+                    this.state.list.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                {/* 引入后直接使用进行跳转就可以，但是需要注意一点，要用{}的形式，也就是把to里边的内容解析成JS的形式，这样才能顺利的传值过去。 */}
+                                <Link to={'/list/' + item.uid}> {item.title}</Link>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        )
     }
 }
 
